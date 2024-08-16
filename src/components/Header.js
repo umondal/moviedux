4320c5eb23467d5a843bc0e02c84fc6711e17409
Udit Mondal <udit91093@hotmail.com>
2024-08-16 17:09:46 -0400

    Add New Header and Footer

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css";
+
+export default function Header() {
+  return (
+    <div>
+      <img className="logo" src="logo.png" alt="moviedux"></img>
+      <h2 className="app-subtitle">
+        It's time for popcorn! Find your next movie here.
+      </h2>
+    </div>
+  );
+}
