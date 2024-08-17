457747d08e776cf12e5509c24ca3f300529668c4
Udit Mondal <udit91093@hotmail.com>
2024-08-17 19:40:28 -0400

    Add Search Bar

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "../styles.css";
+
+export default function () {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movie..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
+}
